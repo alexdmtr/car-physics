ecb86580126c24acd195a88627058052b0bc7885
Alex Dumitru <alexadrian.dmtr@gmail.com>
2016-09-16 11:52:38 +0300

    Revert "Pending work on css"
    This reverts commit 8b9c046c5dbc7924efb8a925ce830aa0a01ce6bc.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mecanica unei masini in miscare' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
