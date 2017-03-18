4dfc415d8b1b37c9e71faec9a8d2a518d8323918
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-03-18 11:39:12 +0200

    Add /about

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mecanica unei masini in miscare' });
 });
 
+/* GET about */
+router.get('/about', function(req, res) {
+  res.render('about', { title: 'Despre'})
+})
+
 module.exports = router;
