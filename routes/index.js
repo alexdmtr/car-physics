9439dc9be6dd0fd7daabb55133c51f4ec7c4be88
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-03-19 14:03:03 +0200

    UI touch-ups

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+function getTitle(title) {
+  let constant = 'Mecanica unei masini in miscare'
+  if (title != null)
+    constant = title + ' | ' + constant
+  return constant
+}
+
+let nav = {
+  'Animație': '/',
+  'Despre': '/about'
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mecanica unei masini in miscare' });
-});
+  res.render('index', { title: getTitle(), nav, page:'/' });
+})
 
 /* GET about */
 router.get('/about', function(req, res) {
-  res.render('about', { title: 'Despre'})
+  res.render('about', { title: getTitle('Despre'), nav, page:'/about'})
 })
 
 module.exports = router;
